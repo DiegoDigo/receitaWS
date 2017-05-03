d2b63f5ab759a683b2735e61fe78550f21163c61
diego.delmiro <di3g0d0ming05@gmail.com>
2017-05-03 14:30:59 -0300

    ajuste na solicitaçao da url

--- src/ClientRest.java
+++ src/ClientRest.java
@@ -1,5 +1,8 @@
 import com.google.gson.Gson;
+import models.AtividadePrincipal;
+import models.AtividadeSecundaria;
 import models.DadosReceita;
+import models.QuadroSocioAdm;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -13,25 +16,25 @@ import java.net.URL;
 
 public class ClientRest {
 
-    private static final String URL_SEFAZ = "http://www.receitaws.com.br/v1/cnpj/27865757000102";
+    private static final String URL_SEFAZ = "http://www.receitaws.com.br/v1/cnpj/";
 
     public static void main(String[] args){
 
-//        if(args.length > 0){
+        if(args.length > 0){
             try {
-                URL url = new URL(URL_SEFAZ);
+                URL url = new URL(URL_SEFAZ + args);
                 HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
                 conexao.setRequestMethod("GET");
                 String conteudo = converterInputStreamParaString(conexao.getInputStream());
-                converterEmObjeto(conteudo, "27865757000102");
+                converterEmObjeto(conteudo, args.toString());
                 conexao.disconnect();
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
-//        }else{
-//            System.out.printf("Por Favor passe o numero do cnpj a pesquisar");
-//        }
+        }else{
+            System.out.printf("Por Favor passe o numero do cnpj a pesquisar");
+        }
     }
 
     public static String converterInputStreamParaString(InputStream is) throws IOException {
@@ -71,14 +74,84 @@ public class ClientRest {
 
         FileWriter arq = new FileWriter(caminho);
         PrintWriter gravarArq = new PrintWriter(arq);
-
-        gravarArq.printf("+--Resultado--+%n");
-
-        gravarArq.println(dados.getAbertura());
-
+        moverDadosParaArquivo(gravarArq,dados);
         arq.close();
 
 
+    }
+
+    private static void moverDadosParaArquivo(PrintWriter gravarArq, DadosReceita dados){
+        gravarArq.printf(dados.getStatus());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getMessage() == null ? "" : dados.getMessage());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getCnpj());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getTipo());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getAbertura());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getNome());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getFantasia());
+        gravarArq.printf(";");
+        for (AtividadePrincipal atividade : dados.getAtividade_principal()) {
+            gravarArq.printf(atividade.getCode());
+            gravarArq.printf(";");
+            gravarArq.printf(atividade.getText());
+            gravarArq.printf(";");
+        }
+        for (AtividadeSecundaria atividade : dados.getAtividades_secundarias()) {
+            gravarArq.printf(atividade.getCode());
+            gravarArq.printf(";");
+            gravarArq.printf(atividade.getText());
+            gravarArq.printf(";");
+        }
+        gravarArq.printf(dados.getNatureza_juridica());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getLogradouro());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getNumero());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getComplemento());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getCep());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getBairro());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getMunicipio());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getUf());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getEmail());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getTelefone());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getEfr());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getSituacao());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getData_situacao());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getMotivo_situacao());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getSituacao_especial());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getData_situacao_especial());
+        gravarArq.printf(";");
+        gravarArq.printf(dados.getCapital_social());
+        gravarArq.printf(";");
+        for (QuadroSocioAdm qsa : dados.getQsa()) {
+            gravarArq.printf(qsa.getNome());
+            gravarArq.printf(";");
+            gravarArq.printf(qsa.getQual());
+            gravarArq.printf(";");
+            gravarArq.printf(qsa.getPais_origim() == null ? "" : qsa.getPais_origim());
+            gravarArq.printf(";");
+            gravarArq.printf(qsa.getNome_rep_legal() == null ? "" : qsa.getNome_rep_legal());
+            gravarArq.printf(";");
+            gravarArq.printf(qsa.getQual_rep_legal() == null ? "" : qsa.getQual_rep_legal());
+        }
     }
 
 }
